ff3ae2b17d32545902d7fbccd28a3126ab358ce9
Haider_Gamerzz <91683909+HaiderGamerz@users.noreply.github.com>
2022-01-23 15:19:10 +0500

    first commit

---
+++ assignment-operators.js
@@ -0,0 +1,14 @@
+// // let x=8
+// // var y=x
+
+// let x=8
+// var y=80
+// console.log(x+y)
+
+//Q4)Add two numbers ( x and y where y is 10 times x ) 
+//then subtract it from 10 and then divide it by 7 
+
+let x=5;
+var y=50;
+
+console.log(y-10/7)
---
+++ data-types.js
@@ -0,0 +1,114 @@
+// 1)Number
+// 2)String 
+// 3)Array
+// 4)Object
+// 5)Empty value
+// 6)Undefined
+// 7)Null
+// 8)Boolean
+
+
+// let mainArray = [1, 2, 3, 4]  // Array
+
+// let x = 9
+// let y = 1
+
+// console.log(x + y)
+
+// var isTrue = true
+// if (!isTrue) {
+//     console.log("This is true")
+// }
+// else {
+//     console.log("this is not true")
+// }
+
+// !isTrue = false
+
+// Array vs object
+// const vehicals = ["Bike", "Cars", "Bus"];
+// console.log(vehicals[0])
+// console.log(vehicals)
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+// };
+// firstName=key 
+// John is th value 
+// console.log(person.firstName)
+// //Q1
+// let students = [
+//     {
+//         id: 1,
+//         studentName: "John",
+//         rollNo: 4,
+//         section: "VD"
+//     },
+//     {
+//         id: 2,
+//         studentName: "Ethan",
+//         rollNo: 2,
+//         section: "VE"
+//     },
+//     {
+//         id: 3,
+//         studentName: "Steve",
+//         rollNo: 10,
+//         section: "VA"
+//     },
+//     {
+//         id: 4,
+//         studentName: "Robert",
+//         rollNo: 2,
+//         section: "VB"
+//     },
+//     {
+//         id: 5,
+//         studentName: "Justin",
+//         rollNo: 10,
+//         section: "VK"
+//     },
+
+// ]
+
+// console.log(students)  // array[0]
+
+//Q2
+let students = [
+    {
+        id: 1,
+        studentName: "John",
+        rollNo: 4,
+        section: "VD"
+    },
+    {
+        id: 2,
+        studentName: "Ethan",
+        rollNo: 2,
+        section: "VE"
+    },
+    {
+        id: 3,
+        studentName: "Steve",
+        rollNo: 10,
+        section: "VA"
+    },
+    {
+        id: 4,
+        studentName: "Robert",
+        rollNo: 2,
+        section: "VB"
+    },
+    {
+        id: 5,
+        studentName: "Justin",
+        rollNo: 10,
+        section: "VK"
+    },
+
+]
+
+console.log(students[1].studentName,students[1].rollNo)
